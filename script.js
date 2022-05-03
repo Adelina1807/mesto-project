89ae30ffe2775a59d24e3a0ca09cbb6355c307da
Adelina Latypova <lat1807@mail.ru>
2022-05-03 19:31:37 +0300

    feat: Добавить плавное открытие и закрытие

--- script.js
+++ script.js
@@ -26,42 +26,42 @@ const initialCards = [
 ];
 
 const cards = [];
-const cardsOpenedTemplate = document.querySelector('.card-opened__template').content;
 const cardTemplate = document.querySelector('.card__template').content;
 const cardSection = document.querySelector('.photos');
-const content = document.querySelector('.content');
+const cardOpened = document.querySelector('.popup_type_photo');
+const cardOpenedImage = cardOpened.querySelector('.card-opened__image');
+const cardOpenedText = cardOpened.querySelector('.card-opened__text');
+const cardOpenedClose = cardOpened.querySelector('.window__close-button_type_photo');
+
+cardOpenedClose.addEventListener('click', function() {
+  cardOpened.classList.remove('popup_opened');
+});
 
 //Добавялем на страницу 6 карточек
 initialCards.forEach(function(item, i) {
-  cards.push({cardClosed: cardTemplate.querySelector('.card').cloneNode(true), cardOpened: cardsOpenedTemplate.querySelector('.popup_type_photo').cloneNode(true)});
+  cards.push(cardTemplate.querySelector('.card').cloneNode(true));
   cards[i];
 
-  cards[i].cardClosed.querySelector('.card__image').src = initialCards[i].link;
-  cards[i].cardClosed.querySelector('.card__image').alt = initialCards[i].name;
-  cards[i].cardClosed.querySelector('.card__name').textContent = initialCards[i].name;
+  cards[i].querySelector('.card__image').src = initialCards[i].link;
+  cards[i].querySelector('.card__image').alt = initialCards[i].name;
+  cards[i].querySelector('.card__name').textContent = initialCards[i].name;
 
-  cards[i].cardOpened.querySelector('.card-opened__image').src = initialCards[i].link;
-  cards[i].cardOpened.querySelector('.card-opened__image').alt = initialCards[i].name;
-  cards[i].cardOpened.querySelector('.card-opened__text').textContent = initialCards[i].name;
-
-  cards[i].cardClosed.querySelector('.card__image').addEventListener('click', function() {
-    cards[i].cardOpened.classList.add('popup_opened');
-  });
-
-  cards[i].cardOpened.querySelector('.window__close-button_type_photo').addEventListener('click', function(evt) {
-    evt.target.closest('.popup_type_photo').classList.remove('popup_opened');
-  });
-
-  cards[i].cardClosed.querySelector('.card__heart').addEventListener('click', function(evt) {
+  cards[i].querySelector('.card__heart').addEventListener('click', function(evt) {
     evt.target.classList.toggle('card__heart_active');
   });
 
-  cards[i].cardClosed.querySelector('.card__delete').addEventListener('click', function(evt) {
+  cards[i].querySelector('.card__delete').addEventListener('click', function(evt) {
     evt.target.closest('.card').remove();
   });
 
-  cardSection.append(cards[i].cardClosed);
-  content.append(cards[i].cardOpened);
+  cards[i].querySelector('.card__image').addEventListener('click', function() {
+    cardOpenedImage.src = initialCards[i].link;
+    cardOpenedImage.name = initialCards[i].name;
+    cardOpenedText.textContent = initialCards[i].name;
+    cardOpened.classList.add('popup_opened');
+  })
+
+  cardSection.append(cards[i]);
 });
 
 //Открытие и закрытие формы для изменения профиля
@@ -118,36 +118,32 @@ formElement[0].addEventListener('submit', formSubmitEdit);
 function formSubmitAdd (evt) {
   evt.preventDefault();
 
-  cards.unshift({cardClosed: cardTemplate.querySelector('.card').cloneNode(true), cardOpened: cardsOpenedTemplate.querySelector('.popup_type_photo').cloneNode(true)});
+  cards.unshift(cardTemplate.querySelector('.card').cloneNode(true));
 
-  cards[0].cardClosed.querySelector('.card__image').src = jobInput[1].value;
-  cards[0].cardClosed.querySelector('.card__image').alt = nameInput[1].value;
-  cards[0].cardClosed.querySelector('.card__name').textContent = nameInput[1].value;
+  cards[0].querySelector('.card__image').src = jobInput[1].value;
+  cards[0].querySelector('.card__image').alt = nameInput[1].value;
+  cards[0].querySelector('.card__name').textContent = nameInput[1].value;
 
-  cards[0].cardOpened.querySelector('.card-opened__image').src = jobInput[1].value;
-  cards[0].cardOpened.querySelector('.card-opened__image').alt = nameInput[1].value;
-  cards[0].cardOpened.querySelector('.card-opened__text').textContent = nameInput[1].value;
-
-  cards[0].cardClosed.querySelector('.card__heart').addEventListener('click', function(evt) {
+  cards[0].querySelector('.card__heart').addEventListener('click', function(evt) {
     evt.target.classList.toggle('card__heart_active');
   });
 
-  cards[0].cardClosed.querySelector('.card__delete').addEventListener('click', function(evt) {
+  cards[0].querySelector('.card__delete').addEventListener('click', function(evt) {
     evt.target.closest('.card').remove();
   });
 
-  cards[0].cardClosed.querySelector('.card__image').addEventListener('click', function() {
-    cards[0].cardOpened.classList.add('popup_opened');
-  });
+  cards[0].querySelector('.card__image').addEventListener('click', function() {
+    cardOpenedImage.src = jobInput[1].value;
+    cardOpenedImage.name = nameInput[1].value;
+    cardOpenedText.textContent = nameInput[1].value;
+    cardOpened.classList.add('popup_opened');
+  })
 
-  cards[0].cardOpened.querySelector('.window__close-button_type_photo').addEventListener('click', function(evt) {
-    evt.target.closest('.popup_type_photo').classList.remove('popup_opened');
-  });
-
-  cardSection.prepend(cards[0].cardClosed);
-  content.append(cards[0].cardOpened);
+  cardSection.prepend(cards[0]);
 
   closePopupAdd();
 };
 
 formElement[1].addEventListener('submit', formSubmitAdd);
+
+
