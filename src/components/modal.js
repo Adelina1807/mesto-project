91a12425433eda537689772eb3aacccfe32c4a60
Adelina Latypova <lat1807@mail.ru>
2022-07-10 20:36:39 +0300

    Сборка Webpack + Валидация форм

---
+++ src/components/modal.js
@@ -0,0 +1,18 @@
+//Меняем профиль
+import {
+  profileName,
+  profileJob,
+  nameInput,
+  jobInput,
+  editPopup,
+} from "../index.js";
+import { closePopup } from "./utils.js";
+
+export function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+
+  closePopup(editPopup);
+}
---
+++ src/index.js
@@ -0,0 +1,176 @@
+import "./styles/index.css";
+
+import { handleProfileFormSubmit } from "./components/modal.js";
+import { enableValidation } from "./components/validate.js";
+import {
+  handleAddCardFormSubmit,
+  addCard,
+  createCard,
+} from "./components/card.js";
+import { openPopup, closePopup } from "./components/utils.js";
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+export const cardTemplate = document.querySelector(".card__template").content;
+export const cardSection = document.querySelector(".photos");
+
+export const cardOpened = document.querySelector(".popup_type_photo");
+export const cardOpenedImage = cardOpened.querySelector(".card-opened__image");
+export const cardOpenedText = cardOpened.querySelector(".card-opened__text");
+const cardOpenedClose = cardOpened.querySelector(
+  ".window__close-button_type_photo"
+);
+
+const formElementProfile = document.querySelector(".window__form_type_profile");
+export const nameInput = formElementProfile.querySelector(
+  ".window__input_type_name"
+);
+export const jobInput = formElementProfile.querySelector(
+  ".window__input_type_info"
+);
+
+export const profileName = document.querySelector(".profile__name");
+export const profileJob = document.querySelector(".profile__text");
+
+const editButton = document.querySelector(".profile__edit-button");
+export const editPopup = document.querySelector(".popup_type_edit");
+const closeEditButton = document.querySelector(
+  ".window__close-button_type_edit"
+);
+
+const formElementAddCard = document.querySelector(".window__form_type_add");
+export const imageNameInput = formElementAddCard.querySelector(
+  ".window__input_type_name"
+);
+export const linkInput = formElementAddCard.querySelector(
+  ".window__input_type_info"
+);
+
+const addButton = document.querySelector(".profile__add-button");
+export const addPopup = document.querySelector(".popup_type_add");
+const closeAddButton = document.querySelector(".window__close-button_type_add");
+
+//Добавялем на страницу 6 карточек
+initialCards.forEach(function (item) {
+  addCard(createCard(item.link, item.name));
+});
+
+//Открытие формы для изменения профиля
+editButton.addEventListener("click", function () {
+  openPopup(editPopup);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
+
+//Закрытие формы для изменения профиля(кнопка крестик)
+closeEditButton.addEventListener("click", function () {
+  closePopup(editPopup);
+});
+
+//Закрытие формы для изменения профиля(клик вне области)
+editPopup.addEventListener("click", function (evt) {
+  const target = evt.target;
+  console.log(target);
+  if (!target.closest(".window") && !target.closest(".window__form")) {
+    closePopup(editPopup);
+  }
+});
+
+//Закрытие формы для изменения профиля(нажатие escape)
+document.addEventListener("keydown", function (evt) {
+  if (evt.key === "Escape") {
+    closePopup(editPopup);
+  }
+});
+
+//Меняем профиль
+formElementProfile.addEventListener("submit", handleProfileFormSubmit);
+
+//Открытие формы для добавления карточки
+addButton.addEventListener("click", function () {
+  openPopup(addPopup);
+  formElementAddCard.reset();
+});
+
+//Закрытие формы для добавления карточки(кнопка крестик)
+closeAddButton.addEventListener("click", function () {
+  closePopup(addPopup);
+});
+
+//Закрытие формы для добавления карточки(клик вне области)
+addPopup.addEventListener("click", function (evt) {
+  const target = evt.target;
+  console.log(target);
+  if (!target.closest(".window") && !target.closest(".window__form")) {
+    closePopup(addPopup);
+  }
+});
+
+//Закрытие формы для добавления карточки(нажатие escape)
+document.addEventListener("keydown", function (evt) {
+  if (evt.key === "Escape") {
+    closePopup(addPopup);
+  }
+});
+
+//Добавление карточки
+formElementAddCard.addEventListener("submit", handleAddCardFormSubmit);
+
+//Закрытие карточки(кнопка крестик)
+cardOpenedClose.addEventListener("click", function () {
+  closePopup(cardOpened);
+});
+
+//Закрытие карточки(клик вне области)
+cardOpened.addEventListener("click", function (evt) {
+  const target = evt.target;
+  console.log(target);
+  if (!target.closest(".window") && !target.closest(".window__form")) {
+    closePopup(cardOpened);
+  }
+});
+
+//Закрытие карточки(нажатие escape)
+document.addEventListener("keydown", function (evt) {
+  if (evt.key === "Escape") {
+    closePopup(cardOpened);
+  }
+});
+
+//Валидация форм
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+enableValidation({
+  formSelector: ".window__form",
+  inputSelector: ".window__input",
+  submitButtonSelector: ".window__save-button",
+  inactiveButtonClass: "window__save-button_inactive",
+  inputErrorClass: "window__input_type_error",
+  errorClass: "window__input-error_active",
+});
